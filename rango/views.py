7eb3301f651c10227cf6b61ac5e8ba5733667672
weihang you <2587213y@studdent.gla.ac.uk>
2021-07-26 17:09:31 +0100

    c4

--- rango/views.py
+++ rango/views.py
@@ -1,4 +1,11 @@
 from django.http import  HttpResponse
 from django.shortcuts import render
+
+
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    return render(request, 'rango/index.html', context=context_dict)
+
+def about(request):
+    context_dict = {'boldmessage': 'This tutorial has been put together by weihang'}
+    return render(request, 'rango/about.html', context=context_dict)
