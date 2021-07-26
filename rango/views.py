0115e95833ef7310a4f6fbb9eb5357afce889217
weihang you <2587213y@studdent.gla.ac.uk>
2021-07-26 18:06:21 +0100

    error fixed

--- rango/views.py
+++ rango/views.py
@@ -7,5 +7,5 @@ def index(request):
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    context_dict = {'boldmessage': 'This tutorial has been put together by weihang'}
-    return render(request, 'rango/about.html', context=context_dict)
+ 
+    return render(request, 'rango/about.html')
